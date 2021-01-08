c0b7d5fd8fbebd648997f5d0fc3d4579b6d963df
xiaodong chen <cxd032404@hotmail.com>
2021-01-08 14:32:14 +0800

    merge

--- V1/function/config_dev.php
+++ V1/function/config_dev.php
@@ -1,8 +1,9 @@
 <?php
+
 $base_config = [
     'site_name'=>"麒麟赛事",
     'api_url'=>'http://dev_lol_api.querylist.cn',//api站点URL
-    'site_url'=>'info.lol_info.com',//本站URl
+    'site_url'=>'dev.lol_info.com',//本站URl
     'game_name'=>"英雄联盟",
     'game'=>"lol",
     'cacheConfig'=>[
@@ -10,6 +11,7 @@ $base_config = [
         'links'=>['prefix'=>"links","expire"=>7200]
     ]
 ];
+
 $additional_config = ['site_description'=> $base_config['site_name'].'致力于服务广大'.$base_config['game_name'].'玩家，为'.$base_config['game_name'].'玩家提供丰富的'.$base_config['game_name'].'游戏攻略、'.$base_config['game_name'].'电子竞技赛事资讯、数据分析及内容解读。',
 ];
 $config = array_merge($base_config,$additional_config);
