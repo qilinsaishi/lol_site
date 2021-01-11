43885640e46386f1e0a28d60f3a62598ddc64efa
xiaodong chen <cxd032404@hotmail.com>
2021-01-11 14:52:55 +0800

    merge

--- V1/function/config_dev.php
+++ V1/function/config_dev.php
@@ -3,7 +3,7 @@
 $base_config = [
     'site_name'=>"麒麟赛事",
     'api_url'=>'http://dev_lol_api.querylist.cn',//api站点URL
-    'site_url'=>'dev.lol_info.com',//本站URl
+    'site_url'=>'http://dev.lol_info.com',//本站URl
     'game_name'=>"英雄联盟",
     'game'=>"lol",
     'cacheConfig'=>[
