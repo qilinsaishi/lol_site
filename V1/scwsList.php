6b7952dd74add4a9de3e7a0c34ae7c4dc1d04e51
xiaodong chen <cxd032404@hotmail.com>
2021-01-22 17:27:54 +0800

    merge

--- V1/scwsList.php
+++ V1/scwsList.php
@@ -12,6 +12,11 @@ $zxtype=($info['type']!="info")?"/strategylist":"/newslist";
 $data = [
     "scwsKeyword"=>[$ids],
     "informationList"=>["dataType"=>"scwsInformaitonList","ids"=>$ids,"game"=>$config['game'],"page"=>$page,"page_size"=>$info['page']['page_size'],/*"type"=>$info['type']=="info"?"1,2,3,5":"4",*/"fields"=>"*"],
+    "totalTeamList"=>["page"=>1,"page_size"=>6,"game"=>$config['game'],"source"=>"cpseo","fields"=>'team_id,team_name,logo',"rand"=>1,"cacheWith"=>"currentPage"],
+    "tournament"=>["page"=>1,"page_size"=>8],
+    "defaultConfig"=>["keys"=>["contact","sitemap"],"fields"=>["name","key","value"]],
+    "links"=>["game"=>$config['game'],"page"=>1,"page_size"=>6],
+    "totalPlayerList"=>["game"=>$config['game'],"page"=>1,"page_size"=>9,"source"=>"cpseo","fields"=>'player_id,player_name,logo',"rand"=>1,"cacheWith"=>"currentPage"],
     "currentPage"=>["name"=>"infoList","type"=>$zxtype,"page"=>$page,"page_size"=>$info['page']['page_size']]
 ];
 $return = curl_post($config['api_get'],json_encode($data),1);
