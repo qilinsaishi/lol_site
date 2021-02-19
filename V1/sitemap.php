377bd3409076c67d5e92cae2bcfc1c8dfd37d36b
xiaodong chen <cxd032404@hotmail.com>
2021-02-19 13:50:41 +0800

    merge

--- V1/sitemap.php
+++ V1/sitemap.php
@@ -1,10 +1,23 @@
 <?php
 require_once "function/init.php";
+
+$urlList = [];
+foreach($config['navList'] as $nav)
+{
+    if(substr($nav['url'],-5)=="list/")
+    {
+        $urlList[] = $config['site_url']."/".$nav['url']."1";
+    }
+    else
+    {
+        $urlList[] = $config['site_url']."/".$nav['url'];
+    }
+}
 $data = [
         "site_id"=>$config['site_id'],
     ];
 $return = curl_post($config['api_sitemap'],json_encode($data),1);
-$urlList = [];
+
 foreach($return as $type => $detail)
 {
     foreach($detail as $key)
@@ -14,12 +27,13 @@ foreach($return as $type => $detail)
 }
 $return = [];
 $return[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset>\n";
+
 foreach($urlList as $url)
 {
-    $return[] = "<url>\n<loc>".$url."</loc>\n<lastmod>".date('Y-m-d')."</lastmod>\n<changefreq>weekly</changefreq>\n<priority>1.0</priority>\n</url>\n";
+    $return[] = "<url>\n<loc>".$url."</loc>\n<lastmod>".date('Y-m-d')."</lastmod>\n<changefreq>daily</changefreq>\n<priority>1.0</priority>\n</url>\n";
 }
 $return[] = '<urlset>';
-$myfile = fopen("sitemap.xml", "w") or die("Unable to open file!");
+$myfile = fopen(dirname(__FILE__)."/sitemap.xml", "w") or die("Unable to open file!");
 $txt = implode($return);
 fwrite($myfile, $txt);
 fclose($myfile);
