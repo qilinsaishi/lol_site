57a011f71c17b43c3d2a3ffbdb80457a261e0567
xuweisong <xuweisong37@126.com>
2021-02-25 18:47:57 +0800

    资讯

--- V1/scwsList.php
+++ V1/scwsList.php
@@ -84,7 +84,7 @@ $info['page']['total_page'] = intval($return['informationList']['count']/$info['
                           </div>
                           <div class="right">
                               <h2><?php echo $value['content']['title'];?></h2>
-                              <p><?php echo $value['content']['content']; ?></p>
+                              <p><?php echo strip_tags(html_entity_decode($value['content']['content'])); ?></p>
                               <div class="more"><span class="more_btn">More</span> <span><?php echo substr((($value["type"]==2)?$value['site_time']:$value['create_time']),0,10);?></span> </div>
                           </div>
                       </a>
