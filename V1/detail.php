916acf47266013afd3e1e2158568f08cc1bfff0b
justin.chen <cxd032404@hotmail.com>
2021-04-26 17:13:57 +0800

    merge

--- V1/detail.php
+++ V1/detail.php
@@ -18,7 +18,7 @@ $data = [
     "currentPage"=>["name"=>"info","id"=>$id,"site_id"=>$config['site_id']]
 ];
 $return = curl_post($config['api_get'],json_encode($data),1);
-if(!isset($return["information"]['data']['id']))
+if(!isset($return["information"]['data']['id'])  || $return["information"]['data']['game'] != $config['game'])
 {
     render404($config);
 }
