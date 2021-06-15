317ed0ec753549ec02883a807d6c0e53f6b5dc04
cxd032404 <cxd032404@hotmail.com>
2021-06-15 19:21:03 +0800

    Update teamList.php

--- V1/teamList.php
+++ V1/teamList.php
@@ -77,7 +77,7 @@ $info['page']['total_page'] = ceil($return['totalTeamList']['count']/$info['page
                         <a href="<?php echo $config['site_url']; ?>/teamdetail/<?php echo $teamInfo['team_id'];?>" title="<?php echo $teamInfo['team_name'];?>" target="_blank">
                             <div>
                                 <?php if(isset($return['defaultConfig']['data']['default_team_img'])){?>
-                                <img lazyload="true" data-original="<?php echo $return['defaultConfig']['data']['default_team_img']['value'];?>" src="<?php echo $teamInfo['logo'];?>" title="<?php echo $teamInfo['team_name'];?>" />
+                                <img lazyload="true" src="<?php echo $return['defaultConfig']['data']['default_team_img']['value'];?>" data-original="<?php echo $teamInfo['logo'];?>" title="<?php echo $teamInfo['team_name'];?>" />
                                 <?php }else{?>
                                 <img src="<?php echo $teamInfo['logo'];?>" title="<?php echo $teamInfo['team_name'];?>" />
                                 <?php }?>
