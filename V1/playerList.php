68d22896c05e0bc14e31965371ce15745b2131e7
cxd032404 <cxd032404@hotmail.com>
2021-06-15 19:23:17 +0800

    Update playerList.php

--- V1/playerList.php
+++ V1/playerList.php
@@ -77,7 +77,7 @@ $info['page']['total_page'] = intval($return['totalPlayerList']['count']/$info['
                                         <a href="<?php echo $config['site_url']; ?>/playerdetail/<?php echo $playerInfo['player_id'];?>" title="<?php echo $playerInfo['player_name'];?>" target="_blank">
                                             <div>
                                                 <?php if(isset($return['defaultConfig']['data']['default_player_img'])){?>
-                                                    <img lazyload="true" data-original="<?php echo $return['defaultConfig']['data']['default_player_img']['value'];?>" src="<?php echo $playerInfo['logo'];?>" title="<?php echo $playerInfo['player_name'];?>" />
+                                                    <img lazyload="true" src="<?php echo $return['defaultConfig']['data']['default_player_img']['value'];?>" data-original="<?php echo $playerInfo['logo'];?>" title="<?php echo $playerInfo['player_name'];?>" />
                                                 <?php }else{?>
                                                     <img src="<?php echo $playerInfo['logo'];?>" title="<?php echo $playerInfo['player_name'];?>" />
                                                 <?php }?>                                            </div>
